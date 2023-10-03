5a25cac2daccbd64279863ef638367c4aa1ea67b
DenisJ <G00403126@atu.ie>
2023-10-03 15:37:42 +0100

    EX

---
+++ src/test/java/ie/atu/helloworldspringboot/HelloWorldSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.helloworldspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
